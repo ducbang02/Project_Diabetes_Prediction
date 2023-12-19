95d4840c13299e278a42f8889f6505482a3cb7fe
ducbang02 <nguyenducbang.uit@gmail.com>
2023-12-19 22:35:23 +0700

    no message

--- main.py
+++ main.py
@@ -25,11 +25,9 @@ age = st.number_input('Age')
 if gl is not None and bp is not None and skinThickness is not None and insulin is not None and bmi is not None and dpf is not None and age is not None:
     if st.button('Predict'):
         input_data_reshaped = np.array([gl, bp, skinThickness, insulin, bmi, dpf, age]).reshape(1,-1)
-        # Chuẩn hóa dữ liệu đầu vào
-        std_data = model.scaler.transform(input_data_reshaped)
-       
+        
         # Máy làm và cho ra kết quả 0 hoặc 1
-        prediction = model.predict(std_data)
+        prediction = model.predict(input_data_reshaped)
     
         st.header('Result')
         
