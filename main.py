5858df8364e2e09d93a4fd45c6a79cf1daed7d5a
ducbang02 <nguyenducbang.uit@gmail.com>
2023-12-19 20:45:13 +0700

    fix main.py

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 import streamlit as st
-import sklearn
+from PIL import Image
 import pickle as pkl
 import numpy as np
 
 
 st.title("Ứng Dụng Dự Đoán Điabetes")
 
-# Load the model
+image = Image.open('DiabetesPredictioninMachineLearningusingPython.jpg')
+st.image(image)
+
 input = open('diabetes.pkl', 'rb')
 model = pkl.load(input)
 
+st.header('Input admission information')
+gl = st.number_input('Glucose')
+bp = st.number_input('BloodPressure')
+skinThickness = st.number_input('SkinThickness')
+insulin = st.number_input('Insulin')
+bmi = st.number_input('BMI')
+dpf = st.number_input('DiabetesPedigreeFunction')
+age = st.number_input('Age')
 
-# Tạo 8 text fields và lưu giá trị vào mảng input_data
-input_data = []
-for i in range(1, 9):
-    value = st.text_input(f"Nhập giá trị {i}:", key=f"value{i}")
-    if value:
-        input_data.append(float(value))
 
-# Kiểm tra nếu đã nhập đủ 8 giá trị
-if len(input_data) == 8:
-    st.success("Đã nhập đủ số lượng.")
-    input_data = np.asarray(input_data).reshape(1, -1)  # Reshape thành mảng 2 chiều
-    # Thực hiện dự đoán
-    prediction = model.predict(input_data)
-    # Hiển thị kết quả
-    st.write("Kết quả dự đoán:")
-    if prediction[0] == 0:
-        st.write('The person is not diabetic')
-    else:
-        st.write('The person is diabetic')
-else:
-    st.warning("Hãy nhập đủ số lượng.")
+if gl is not None and bp is not None and s is not None and insulin is not None and bmi is not None and dpf is not None and age is not None:
+    if st.button('Predict'):
+        feature_vector = np.array([gl, bp, skinThickness, insulin, bmi, dpf, age]).reshape(1,-1)
+        result = str((model.predict(feature_vector)[0])[0])
+
+        st.header('Result')
+        st.text(result)
